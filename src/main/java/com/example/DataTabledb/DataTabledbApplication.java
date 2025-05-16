b97ad4b24047a2924868f5f2b167f941b87ab58d
vishwa <2810vishwa@gmail.com>
2025-05-16 10:38:27 +0530

    project added

---
+++ src/main/java/com/example/DataTabledb/DataTabledbApplication.java
@@ -0,0 +1,13 @@
+package com.example.DataTabledb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataTabledbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataTabledbApplication.class, args);
+	}
+
+}
