42dfc9a154fa3bcf8e0b2dd38c81456345623ea1
vishwa <2810vishwa@gmail.com>
2025-07-08 21:22:14 +0530

    Update AuthService.java

--- src/main/java/com/example/DataTabledb/service/AuthService.java
+++ src/main/java/com/example/DataTabledb/service/AuthService.java
@@ -2,7 +2,6 @@ package com.example.DataTabledb.service;
 
 import com.example.DataTabledb.repository.UserRepository;
 import com.example.DataTabledb.model.User;
-import com.example.DataTabledb.repository.UserRepository;
 import com.example.DataTabledb.security.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
